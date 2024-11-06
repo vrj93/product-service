71297a4c5e93b8db176fa79fe19c705282d1aad0
Vivek Joshi <vrj022@gmail.com>
2024-11-06 18:11:14 +0530

    Folder refactoring.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './module/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from './schema/product.schema';
+import { Category, CategorySchema } from './schema/category.schema';
+import { Brand, BrandSchema } from './schema/brand.schema';
+import { Review, ReviewSchema } from './schema/review.schema';
+import { Inventory, InventorySchema } from './schema/inventory.schema';
+import { Price, PriceSchema } from './schema/price.schema';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://product-mongo-svc:27017/product-service'),
-    ProductModule,
+    MongooseModule.forFeature([
+      { name: Product.name, schema: ProductSchema },
+      { name: Category.name, schema: CategorySchema },
+      { name: Brand.name, schema: BrandSchema },
+      { name: Review.name, schema: ReviewSchema },
+      { name: Inventory.name, schema: InventorySchema },
+      { name: Price.name, schema: PriceSchema },
+    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
