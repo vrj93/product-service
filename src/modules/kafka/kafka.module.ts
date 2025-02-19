97b92f26704b072e2b1f36c797d04691064039e2
Vivek Joshi <vrj022@gmail.com>
2025-02-19 19:10:45 +0530

    Elastice integration removed.

--- src/modules/kafka/kafka.module.ts
+++ src/modules/kafka/kafka.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { KafkaService } from './kafka.service';
 import { KafkaConsumerService } from './kafka-consumer.service';
-import { ElasticSearchModule } from '../elasticsearch/elasticsearch.module';
 import { MongoModule } from '../mongo/mongo.module';
 
 @Module({
-  imports: [MongoModule, ElasticSearchModule],
+  imports: [MongoModule],
   providers: [KafkaService, KafkaConsumerService],
   exports: [KafkaService, KafkaConsumerService],
 })
